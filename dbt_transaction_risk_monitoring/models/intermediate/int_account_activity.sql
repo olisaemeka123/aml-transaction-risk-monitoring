b92d9b76229835cef2d17c0a2e2cf627cced02cd
Onyia Olisaemeka <Onyiaolisaemeka51@gmail.com>
2026-09-15 17:22:03 +0100

    fix: resolve sqlfluff RF04/LT05 lint failures in intermediate models

--- dbt_transaction_risk_monitoring/models/intermediate/int_account_activity.sql
+++ dbt_transaction_risk_monitoring/models/intermediate/int_account_activity.sql
@@ -1,25 +1,27 @@
 with as_sender as (
 
-select
-    sender_account_id as account_id,
-    transaction_id,
-    transaction_ts,
-    amount,
-    'sender' as role
+    select
+        sender_account_id as account_id,
+        transaction_id,
+        transaction_ts,
+        amount,
+        'sender' as account_role
 
-from {{ ref('stg_transactions') }}
+    from {{ ref('stg_transactions') }}
 
 ),
 
-as_receiver as(
+as_receiver as (
+
     select
         receiver_account_id as account_id,
         transaction_id,
         transaction_ts,
         amount,
-        'receiver' as role
+        'receiver' as account_role
 
     from {{ ref('stg_transactions') }}
+
 )
 
 select * from as_sender
--- dbt_transaction_risk_monitoring/models/intermediate/int_account_velocity.sql
+++ dbt_transaction_risk_monitoring/models/intermediate/int_account_velocity.sql
@@ -1,32 +1,34 @@
 with velocity_count as (
 
-select
-    account_id,
-    transaction_id,
-    transaction_ts,
-    amount,
-    role,
- 
- -- converting timestamp field to numeric using unix seconds() to allow for range-based windowing, since big query doesn't allow use of timestamp in order by clause in window functions.
-     count(*) over (
-        partition by account_id
-        order by unix_seconds(transaction_ts)
-        range between 3600 preceding and current row  -- 1 hour, in seconds
-    ) as txn_count_last_1h,
- 
-    count(*) over (
-        partition by account_id
-        order by unix_seconds(transaction_ts)
-        range between 86400 preceding and current row  -- 24 hours, in seconds
-    ) as txn_count_last_24h,
- 
-    count(*) over (
-        partition by account_id
-        order by unix_seconds(transaction_ts)
-        range between 604800 preceding and current row  -- 7 days, in seconds
-    ) as txn_count_last_7d
+    select
+        account_id,
+        transaction_id,
+        transaction_ts,
+        amount,
+        account_role,
 
-from {{ref('int_account_activity')}}
+        -- unix_seconds() avoids BigQuery's TIMESTAMP-in-RANGE restriction
+        count(*) over (
+            partition by account_id
+            order by unix_seconds(transaction_ts)
+            range between 3600 preceding and current row  -- 1 hour, in seconds
+        ) as txn_count_last_1h,
+
+        count(*) over (
+            partition by account_id
+            order by unix_seconds(transaction_ts)
+            -- 24 hours, in seconds
+            range between 86400 preceding and current row
+        ) as txn_count_last_24h,
+
+        count(*) over (
+            partition by account_id
+            order by unix_seconds(transaction_ts)
+            -- 7 days, in seconds
+            range between 604800 preceding and current row
+        ) as txn_count_last_7d
+
+    from {{ ref('int_account_activity') }}
 
 )
 
